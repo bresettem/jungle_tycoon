42bd4381c71a2664581cb5ff1ffcdde683117775
Matthew <bresettem@users.noreply.github.com>
2022-04-29 00:21:41 -0500

    Added template

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,11 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
+// Custom
+import './custom/mdb.min'
+import "@fortawesome/fontawesome-free/js/fontawesome"
+import "@fortawesome/fontawesome-free/js/solid"
+
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
