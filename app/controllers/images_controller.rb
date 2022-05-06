482efdcaafc8670875b1c58b1bd98c51238e4166
Matthew <bresettem@users.noreply.github.com>
2022-05-05 20:55:41 -0500

    Added royals

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,6 @@ class ImagesController < ApplicationController
   def index
     if params[:query].present?
       replace = params[:query].gsub(',', ' ')
-      # 54
       @pagy, @images = pagy(Image.order(identifier: :asc).global_search(replace), items: 54)
     else
       @pagy, @images = pagy(Image.order(identifier: :asc), items: 54)
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -21,4 +21,9 @@ let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 })
 $(document).on("turbo:load", function () {
     console.log("turbo!");
+    $("#royals").click(function (event) {
+        let royals = '2 60 249 350 366 556 577 584 618 731 793 969 1443 1699 1720 1858 1887 2100 2527 2881 3016 3323 ' +
+            '3398 3412 3446 3492 3533 3552 3662 3687 3735 3864 3907 3925 3932 4017 4085 4130 4201 4404'
+        $('#query').val(royals)
+    });
 });
